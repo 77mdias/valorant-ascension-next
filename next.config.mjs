67d13dd33b4b9948a2f83f7ce19673027500c3d5
77mdias <jean.carlos3@outlook.com>
2025-11-15 14:18:44 -0300

    fix: correçoes de configuraçao de remote patterns de imagens.

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,58 @@ const nextConfig = {
       bodySizeLimit: '2mb',
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static.valorantzone.gg",
+      },
+      {
+        protocol: "https",
+        hostname: "noticias.maisesports.com.br",
+      },
+      {
+        protocol: "https",
+        hostname: "s2-techtudo.glbimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "s2-ge.glbimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.thespike.gg",
+      },
+      {
+        protocol: "https",
+        hostname: "static.wikia.nocookie.net",
+      },
+      {
+        protocol: "https",
+        hostname: "cmsassets.rgpub.io",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.valorant-api.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
   async headers() {
     return [
       {
